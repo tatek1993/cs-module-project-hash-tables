acb3e85134982ffcbf6964aa9b4cf432eb7a55b1
Tate Keller <tatek1993@gmail.com>
2020-08-05 18:24:30 -0500

    tests for expensive_seq.py passing

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,26 @@
 # Your code here
 
+# exps(x, y, z) =
+#      if x <= 0: y + z
+#      if x >  0: exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
+cache = {}
+
 
 def expensive_seq(x, y, z):
     # Your code here
+    keys = (x, y, z)
+    if x <= 0:
+        return y + z
 
+    if keys not in cache:
+        cache[keys] = expensive_seq(x - 1, y + 1, z) + expensive_seq(
+            x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+    return cache[keys]
 
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+        x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
