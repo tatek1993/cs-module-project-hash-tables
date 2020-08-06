35fe028d162b46fc73d3bd6e0a48eb62baf2bd9e
Tate Keller <tatek1993@gmail.com>
2020-08-05 22:22:22 -0500

    tests for word_count.py passing

--- README.md
+++ README.md
@@ -73,8 +73,8 @@ For these, you can use either the built-in `dict` type, or the hashtable
 you built. (Some of these are easier with `dict` since it's more
 full-featured.)
 
-* [Lookup Table](applications/lookup_table/)
-* [Expensive Sequence](applications/expensive_seq/)
+<!-- * [Lookup Table](applications/lookup_table/) -->
+<!-- * [Expensive Sequence](applications/expensive_seq/) -->
 * [Word Count](applications/word_count/)
 * [No Duplicates](applications/no_dups/)
 * [Markov Chains](applications/markov/)
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,40 @@
 def word_count(s):
     # Your code here
+    # create an array of all characters to ignore
+    ignore = [
+        '"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{',
+        '}', '(', ')', '*', '^', '&', '\t', '\r', '\n'
+    ]
 
+    # start a count
+    cache = {}
+    # if a character from the ignore array is present in the string replace it with white space
+    for character in ignore:
+        s = s.replace(character, ' ')
+
+    # split the newly cleaned & lowercase string by white space
+    split_string = s.lower().split(' ')
+
+    # iterate through the new split string
+    for word in split_string:
+        # if the word is an empty space, skip it
+        if word == '':
+            pass
+        # if the word isn't present in cache then add it
+        elif word not in cache:
+            cache[word] = 1
+        # otherwise increment
+        else:
+            cache[word] += 1
+    print(cache)
+    return cache
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(
+        word_count(
+            'This is a test of the emergency broadcast network. This is only a test.'
+        ))
